319504ae2396c1ce44043bde394cdf04364e432c
SeoJimin1234 <113419021+SeoJimin1234@users.noreply.github.com>
2025-01-21 17:12:39 +0900

    Docs: Response&ErrorHandling (#9)

---
+++ src/main/java/TokenUs/TokenUs_BE/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,10 @@
+package TokenUs.TokenUs_BE.apiPayload.code;
+
+//구체화하는 Status에서 두개의 메소드를 반드시 Override할 것을 강제하는 역할
+
+public interface BaseErrorCode {
+
+    ErrorReasonDTO getReason();
+
+    ErrorReasonDTO getReasonHttpStatus();
+}
