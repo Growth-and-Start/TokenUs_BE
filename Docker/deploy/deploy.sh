af4c315c95af2b0ad8e29838051e0683a6d7ae92
SeoJimin1234 <113419021+SeoJimin1234@users.noreply.github.com>
2025-03-17 00:42:16 +0900

    deploy (#38)
    * Add CI script for Dev (#13)
    
    * chore/CICD/12 (#14)
    
    * Add CI script for Dev
    
    * chore/12/CICD
    
    * add build
    
    * BuldTestReTry
    
    * thirdTrialforBuildTest
    
    * fourthTrialforTest
    
    * fifthTrialforBuldTest
    
    * Delete build directory
    
    * Delete .github/workflows/dev-deploy.yml
    
    * Feat/signin login/#15 (#18)
    
    * Docs/SetSwagger#15/
    
    * feat/UserSignup/#15
    
    * fix/SignupBug/#16 (#19)
    
    * Feat/for connect flask (#21)
    
    * fix/SignupBug/#16
    
    * feat/uploadVideo
    
    * chore/modifyGradle
    
    * feat/17/login (#22)
    
    * fix/loginurl Security Setting
    
    * feat/#25/uploadS3 (#28)
    
    * feat/#25/uploadS3
    
    * fix/#25/gitAction
    
    * Update README.md
    
    * Update README.md
    
    * feat/#26/connectFlask (#29)
    
    * feat/#26/connectFlask
    
    * feat/#26/resultFromFlask
    
    * Feat/#31/connectToTruffle1 (#32)
    
    * feat/#31/connectToTruffle1
    
    * fix/forBuildingGradle
    
    * add gitignore
    
    * Delete .DS_Store
    
    * feat/modify Description on API
    
    * fix/test code
    
    * feat/#31/deploy (#37)
    
    * Feat/#31/deploy (#39)
    
    * feat/#31/deploy
    
    * modify env setting
    
    * Feat/#31/deploy (#40)
    
    * feat/#31/deploy
    
    * modify env setting
    
    * modify script
    
    * Feat/#31/deploy (#41)
    
    * feat/#31/deploy
    
    * modify env setting
    
    * modify script
    
    * modify2
    
    * Feat/#31/deploy (#42)
    
    * feat/#31/deploy
    
    * modify env setting
    
    * modify script
    
    * modify2
    
    * bugfix/dockercompose file

--- Docker/deploy/deploy.sh
+++ Docker/deploy/deploy.sh
@@ -1,15 +1,19 @@
-# 이 파일 내용은 모두 임시입니다.
+# AWS ECR 로그인
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 343218215881.dkr.ecr.ap-northeast-2.amazonaws.com
 
 # ECR에서 최신 이미지 pull
 echo "ECR에 있는 이미지 불러오기"
-if ! docker pull 061039770972.dkr.ecr.ap-northeast-2.amazonaws.com/tokenus-docker:latest; then
+if ! docker pull 343218215881.dkr.ecr.ap-northeast-2.amazonaws.com/tokenus/app:latest; then
     echo "이미지 불러오기에 실패했습니다."
     exit 1
 fi
 
 # Docker compose down으로 기존 컨테이너 중지 및 삭제
 echo "Docker compose down 실행"
-docker compose down
+docker compose down --remove-orphans
+
+# 볼륨이 없으면 생성
+docker volume create mysql_data || true
 
 # Docker compose up 실행
 echo "Docker compose up 실행"
