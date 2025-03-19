ebf211445d51d47f18f301f494f735594adb7958
SeoJimin1234 <113419021+SeoJimin1234@users.noreply.github.com>
2025-03-19 22:45:28 +0900

    Chore/#52/set cors (#56)
    * chore/setCORS
    
    * change cors port number

--- src/main/java/TokenUs/TokenUs_BE/config/CorsConfig.java
+++ src/main/java/TokenUs/TokenUs_BE/config/CorsConfig.java
@@ -15,11 +15,12 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
                         .allowedOrigins(
-                                "http://localhost:3000" // 로컬 프론트엔드 개발 환경, 배포 후 변경 필요
+                                "http://localhost:5173" // 로컬 프론트엔드 개발 환경, 배포 후 변경 필요
                                 )
-                        .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*") // 모든 헤더 허용
-                        .allowCredentials(true); // JWT 쿠키/토큰 포함 허용, allowOrigins("*")불가
+                        .allowedMethods("*") // ✅ 모든 HTTP 메서드 허용
+                        .allowedHeaders("*") // ✅ 모든 요청 헤더 허용
+                        .allowCredentials(true) // JWT 쿠키/토큰 포함 허용, allowOrigins("*")불가
+                        .exposedHeaders("Authorization"); // ✅ JWT 토큰이 담긴 응답 헤더 허용
             }
         };
     }
