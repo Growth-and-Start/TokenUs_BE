26984dc4dece9ac1410ab5f3f8158fec1e6d8faa
SeoJimin <jnuni1004@naver.com>
2025-04-07 17:38:27 +0900

    feat/#125/FAISS

---
+++ src/main/java/TokenUs/TokenUs_BE/controller/FlaskController.java
@@ -0,0 +1,31 @@
+package TokenUs.TokenUs_BE.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import TokenUs.TokenUs_BE.sevice.FlaskService;
+
+@RestController
+@RequestMapping("/flask")
+public class FlaskController {
+    private final FlaskService flaskService;
+
+    public FlaskController(FlaskService flaskService) {
+        this.flaskService = flaskService;
+    }
+
+    @GetMapping("/info")
+    public ResponseEntity<String> getFaissInfo() {
+        String result = flaskService.getFaissInfo();
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/reset")
+    public ResponseEntity<String> resetFaissIndex() {
+        String result = flaskService.resetFaissIndex();
+        return ResponseEntity.ok(result);
+    }
+}
